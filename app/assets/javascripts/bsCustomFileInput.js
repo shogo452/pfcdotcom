9bc598b5daa9a2d2e819cce96119af89962c98b1
tiphp452 <tiphp452@gmail.com>
2020-11-09 22:14:20 +0900

    update some files

--- app/assets/javascripts/bsCustomFileInput.js
+++
@@ -1,6 +0,0 @@
-bsCustomFileInput.init();
-document.getElementById('inputFileReset').addEventListener('click', function() {
-  var elem = document.getElementById('inputFile');
-  elem.value = '';
-  elem.dispatchEvent(new Event('change'));
-});
