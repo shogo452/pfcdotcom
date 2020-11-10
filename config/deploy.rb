09320e30945422d87a87e819c700b62b7dfb9f3a
tiphp452 <tiphp452@gmail.com>
2020-11-10 20:49:13 +0900

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,8 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/my-key.pem'] 
 
