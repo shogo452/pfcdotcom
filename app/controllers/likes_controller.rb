a53c66876b3758793b23acda36fe93dcba663c91
tiphp452 <tiphp452@gmail.com>
2020-11-22 21:10:26 +0900

    #152

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,18 @@
 class LikesController < ApplicationController
+  before_action :set_product
 
   def create
     @like = current_user.likes.create(product_id: params[:product_id])
-    @product = Product.find(params[:product_id])
     @product.created_like_notification_by(current_user)
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @like = Like.find_by(product_id: params[:product_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:product_id])
   end
 end
